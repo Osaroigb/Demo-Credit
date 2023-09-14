c1c3676c9f79565fc0da69a51d1716e33d7bb436
Osaro Igbinovia <emmyigbinovia@gmail.com>
2023-09-14 11:39:09 +0100

    fix: change logging format for vercel

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+// import fs from 'fs';
 import config from '../config';
 import jsonStringify from 'safe-json-stringify';
 import { createLogger, transports, format } from 'winston';
@@ -6,20 +6,36 @@ import { createLogger, transports, format } from 'winston';
 const timestampDefinition = { format: 'YYYY-MM-DDTHH:mm:ss.SSS Z' };
 
 // Check if the logs directory exists, and create it if not
-const logDir = 'logs';
+// const logDir = 'logs';
 
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
+// if (!fs.existsSync(logDir)) {
+//   fs.mkdirSync(logDir);
+// }
 
 const logger = createLogger({
-  level: 'info',
-  format: format.combine(format.timestamp(timestampDefinition), format.json()),
+  level: 'info', // Set the default log level to info
+  format: format.combine(format.timestamp(), format.json()), // Customize the log format
   transports: [
-    new transports.File({ filename: 'logs/error.log', level: 'error', silent: process.env.NODE_ENV === 'test' }),
-    new transports.File({ filename: 'logs/combined.log', silent: process.env.NODE_ENV === 'test' })
-  ]
+    // Log info messages to stdout
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()), // Customize the log format for info messages
+    }),
+    // Log error messages to stderr
+    new transports.Console({
+      level: 'error', // Set the log level for this transport to error
+      format: format.combine(format.colorize(), format.simple()), // Customize the log format for error messages
+    }),
+  ],
 });
+
+// const logger = createLogger({
+//   level: 'info',
+//   format: format.combine(format.timestamp(timestampDefinition), format.json()),
+//   transports: [
+//     new transports.File({ filename: 'logs/error.log', level: 'error', silent: process.env.NODE_ENV === 'test' }),
+//     new transports.File({ filename: 'logs/combined.log', silent: process.env.NODE_ENV === 'test' })
+//   ]
+// });
 
 const showLogs = config.get('showLogs');
 
