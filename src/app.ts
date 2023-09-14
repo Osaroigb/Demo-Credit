83005e8b0d713c2ad6a95eb4372c56b570eb4c3a
Osaro Igbinovia <emmyigbinovia@gmail.com>
2023-09-14 14:26:19 +0100

    refactor: upgraded cdn version

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const swaggerSpecs = require('../swagger');
 const swaggerUi = require('swagger-ui-express');
 
 const app = express();
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs, { customCssUrl: CSS_URL }));
 
 app.use(cors());
